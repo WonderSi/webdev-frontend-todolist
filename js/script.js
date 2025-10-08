fc803406b2442a041ae244a60cf32257b3e59b28
WonderSi <kirillkozlov00@gmail.com>
2025-10-08 10:37:20 +0700

    update and added function applyCurrentFilter

--- js/script.js
+++ js/script.js
@@ -7,6 +7,40 @@ $(document).ready(function () {
     const $taskList = $('.todo-list__tasks');
     const $searchInput = $('.search-bar__input');
 
+    function addTaskToList(task, completed = false) {
+        const taskId = 'task-' + Date.now();
+        const completedClass = completed ? 'task-item--completed' : '';
+        const checkedAttr = completed ? 'checked' : '';
+
+        const taskHtml = 
+        `
+        <li class="task-item">
+            <input type="checkbox" class="task-item__checkbox" id="${taskId}">
+            <span for="${taskId}" class="task-item__span">${taskText}</span>
+            <div class="task-item__actions">
+            <button class="task-item__action task-item__action--edit" aria-label="Edit">
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+            </button>
+            <button class="task-item__action task-item__action--delete" aria-label="Delete">
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z" stroke="currentColor"/>
+                <path d="M14.625 3.75H3.375" stroke="currentColor" stroke-linecap="round"/>
+                <path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z" stroke="currentColor"/>
+                <path d="M10.5 9V12.75" stroke="currentColor" stroke-linecap="round"/>
+                <path d="M7.5 9V12.75" stroke="currentColor" stroke-linecap="round"/>
+                </svg>
+            </button>
+            </div>
+        </li>
+        `;
+
+        $taskList.append(taskHtml);
+        
+        updateEmptyState();
+    }
+
     function updateEmptyState() {
         const $tasks = $('.todo-list__tasks');
         const $empty = $('.todo-list__empty');
@@ -20,6 +54,27 @@ $(document).ready(function () {
             $tasks.show();
             $empty.hide();
         }
+    }
+
+    function applyCurrentFilter() {
+        const $tasks = $('.task-item');
+
+        $tasks.each(function() {
+            const $task = $(this);
+            const isCompleted = $task.hasClass('task-item--completed');
+                    
+            switch(currentFilter) {
+                case 'all':
+                    $task.show();
+                    break;
+                case 'completed':
+                    isCompleted ? $task.show() : $task.hide();
+                    break;
+                case 'incomplete':
+                    !isCompleted ? $task.show() : $task.hide();
+                    break;
+            }
+        });
     }
 
     updateEmptyState();
@@ -47,40 +102,13 @@ $(document).ready(function () {
         const taskText = $taskInput.val().trim();
 
         if (taskText === '') {
-            alert('Please enter a task')
+            alert('Please enter a task');
             return;
         }
 
-        const taskId = 'task-' + Date.now();
-
-        const taskHtml = 
-        `
-        <li class="task-item">
-            <input type="checkbox" class="task-item__checkbox" id="${taskId}">
-            <span for="${taskId}" class="task-item__span">${taskText}</span>
-            <div class="task-item__actions">
-            <button class="task-item__action task-item__action--edit" aria-label="Edit">
-                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M8.67272 5.99106L2 12.6637V16H5.33636L12.0091 9.32736M8.67272 5.99106L11.0654 3.59837L11.0669 3.59695C11.3962 3.26759 11.5612 3.10261 11.7514 3.04082C11.9189 2.98639 12.0993 2.98639 12.2669 3.04082C12.4569 3.10257 12.6217 3.26735 12.9506 3.59625L14.4018 5.04738C14.7321 5.37769 14.8973 5.54292 14.9592 5.73337C15.0136 5.90088 15.0136 6.08133 14.9592 6.24885C14.8974 6.43916 14.7324 6.60414 14.4025 6.93398L14.4018 6.93468L12.0091 9.32736M8.67272 5.99106L12.0091 9.32736" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </button>
-            <button class="task-item__action task-item__action--delete" aria-label="Delete">
-                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3.87414 7.61505C3.80712 6.74386 4.49595 6 5.36971 6H12.63C13.5039 6 14.1927 6.74385 14.1257 7.61505L13.6064 14.365C13.5463 15.1465 12.8946 15.75 12.1108 15.75H5.88894C5.10514 15.75 4.45348 15.1465 4.39336 14.365L3.87414 7.61505Z" stroke="currentColor"/>
-                <path d="M14.625 3.75H3.375" stroke="currentColor" stroke-linecap="round"/>
-                <path d="M7.5 2.25C7.5 1.83579 7.83577 1.5 8.25 1.5H9.75C10.1642 1.5 10.5 1.83579 10.5 2.25V3.75H7.5V2.25Z" stroke="currentColor"/>
-                <path d="M10.5 9V12.75" stroke="currentColor" stroke-linecap="round"/>
-                <path d="M7.5 9V12.75" stroke="currentColor" stroke-linecap="round"/>
-                </svg>
-            </button>
-            </div>
-        </li>
-        `
-
-        $taskList.append(taskHtml);
+        addTaskToList(taskText);
         $modal.hide();
-        $taskInput.val('');
-        updateEmptyState();
+        $taskInput.val('')
     })
 
     $taskList.on('click', '.task-item__action--delete', function () {
@@ -93,6 +121,7 @@ $(document).ready(function () {
 
         setTimeout(() => {
             $taskItem.remove();
+            applyCurrentFilter();
         }, 300);
         updateEmptyState();
     });
@@ -105,15 +134,15 @@ $(document).ready(function () {
             return;
         }
 
-        const $label = $item.find('.task-item__span');
-        const originalText = $label.text().trim();
+        const $span = $item.find('.task-item__span');
+        const originalText = $span.text().trim();
 
         const $input = $('<input>')
             .attr('type', 'text')
             .addClass('task-item__edit-input')
             .val(originalText)
 
-        $label.replaceWith($input);
+        $span.replaceWith($input);
         $input.focus();
 
         const val = $input.val();
@@ -123,12 +152,12 @@ $(document).ready(function () {
             let newText = $input.val().trim();
             if (newText === '') newText = originalText;
 
-            const $newLabel = $('<span>')
+            const $newSpan = $('<span>')
             .attr('for', $item.find('.task-item__checkbox').attr('id'))
             .addClass('task-item__span')
             .text(newText);
 
-            $input.replaceWith($newLabel);
+            $input.replaceWith($newSpan);
         };
 
         $input.on('blur', saveEdit);
@@ -153,6 +182,7 @@ $(document).ready(function () {
         } else {
             $item.removeClass('task-item--completed');
         }
+        applyCurrentFilter();
     });
 
 
@@ -174,6 +204,8 @@ $(document).ready(function () {
 
     updateEmptyState();
     });
+
+    
 
 
 })
